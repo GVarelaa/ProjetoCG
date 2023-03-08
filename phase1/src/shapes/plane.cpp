328cab834e638608673010f20515c9a1824238a0
Guilherme Varela <guilherme.varelaaa@gmail.com>
2023-03-08 17:21:16 +0000

    plano com indices localidade

--- phase1/src/shapes/plane.cpp
+++ phase1/src/shapes/plane.cpp
@@ -28,18 +28,28 @@ Model generatePlane(float length, int divisions){
     float x = initialX;
     float z = initialZ;
 
-    for(int i = 0; i <= divisions; i++){
+    for(int i = 0; i < divisions; i++){
         x = initialX;
         z = initialZ - i*squareLength;
         
-        for(int j = 0; j <= divisions; j++){
+        for(int j = 0; j < divisions; j++){
             x = initialX - j*squareLength;
-            vertices.push_back(Point(x, 0, z));
 
-            if (i != divisions && j != divisions){
-                vector<Triangle> squareTriangles = generateSquare(i, j, divisions);
-                triangles.insert(triangles.end(), squareTriangles.begin(), squareTriangles.end());
-            }
+            Point p1 = Point(x, 0, z);
+            Point p2 = Point(x, 0, z-squareLength);
+            Point p3 = Point(x-squareLength, 0, z-squareLength);
+            Point p4 = Point(x-squareLength, 0, z);
+
+            vertices.push_back(p1);
+            vertices.push_back(p2);
+            vertices.push_back(p3);
+            vertices.push_back(p4);
+
+            Triangle t1 = Triangle(i*4*divisions + 4*j, i*4*divisions + 4*j + 1, i*4*divisions + 4*j + 2);
+            Triangle t2 = Triangle(i*4*divisions + 4*j, i*4*divisions + 4*j + 2, i*4*divisions + 4*j + 3);
+
+            triangles.push_back(t1);
+            triangles.push_back(t2);
         }
     }
 

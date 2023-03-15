7595c421ae47e41b37e4ed538c998e92175d8c3e
Guilherme Varela <guilherme.varelaaa@gmail.com>
2023-03-15 19:29:30 +0000

    torus com indices feito

--- phase2/src/shapes/torus.cpp
+++ phase2/src/shapes/torus.cpp
@@ -9,19 +9,17 @@ Model generateTorus(float radiusIn, float radiusOut, int slices, int layers){
 
     for(int i = 0; i < slices; i++){
         for(int j = 0; j <= layers; j++){
-            float x = (radiusIn + radiusOut*cos(j*layerAngle))*cos(i*sliceAngle);
-            float y = (radiusIn + radiusOut*cos(j*layerAngle))*sin(i*sliceAngle);
-            float z = radiusOut * sin(j*layerAngle);
-
-            Point p1 = Point((radiusIn + radiusOut*cos(j*layerAngle))*cos(i*sliceAngle), (radiusIn + radiusOut*cos(j*layerAngle))*sin(i*sliceAngle), radiusOut * sin(j*layerAngle));
-            Point p2 = Point((radiusIn + radiusOut*cos(j*layerAngle))*cos((i+1)*sliceAngle), (radiusIn + radiusOut*cos(j*layerAngle))*sin((i+1)*sliceAngle), radiusOut * sin(j*layerAngle));
+            Point p1 = Point((radiusOut + radiusIn*cos(j*layerAngle))*cos(i*sliceAngle), radiusIn * sin(j*layerAngle), (radiusOut + radiusIn*cos(j*layerAngle))*sin(i*sliceAngle));
+            Point p2 = Point((radiusOut + radiusIn*cos(j*layerAngle))*cos((i+1)*sliceAngle), radiusIn * sin(j*layerAngle), (radiusOut + radiusIn*cos(j*layerAngle))*sin((i+1)*sliceAngle));
 
             vertices.push_back(p1);
             vertices.push_back(p2);
-
             if(j!=layers){
-                Triangle t1 = Triangle(i*(2*layers) + j*2, i*(2*layers) + j*2 + 1, i*(2*layers) + j*2 + 2);
-                Triangle t2 = Triangle(i*(2*layers) + j*2 + 1, i*(2*layers) + j*2 + 3, i*(2*layers) + j*2 + 2);
+                Triangle t1 = Triangle(i*(2*layers + 2) + j*2, i*(2*layers + 2) + j*2 + 1, i*(2*layers + 2) + j*2 + 2);
+                Triangle t2 = Triangle(i*(2*layers + 2) + j*2 + 1, i*(2*layers + 2) + j*2 + 3, i*(2*layers + 2) + j*2 + 2);
+
+                triangles.push_back(t1);
+                triangles.push_back(t2);
             }
 
         }

a37c7dca6afc98be9eeef8619f156fc5bb3daf58
gabrielasfc <gsfcunha@gmail.com>
2023-03-15 18:08:58 +0000

    added phase2 folder + torus regex

---
+++ phase2/include/torus.h
@@ -0,0 +1,14 @@
+#ifndef __TORUS__
+#define __TORUS__
+
+#include <vector>
+#include <math.h>
+#include "point.h"
+#include "triangle.h"
+#include "model.h"
+
+using namespace std;
+
+Model generateTorus(float radiusIn, float radiusOut, int slices, int layers);
+
+#endif

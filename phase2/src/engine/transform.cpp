81af6ae86c1a1dbc6e8fb18320923a651a8b6d86
gabrielasfc <gsfcunha@gmail.com>
2023-03-18 18:33:13 +0000

    constructors by xml element

--- phase2/src/engine/transform.cpp
+++ phase2/src/engine/transform.cpp
@@ -7,7 +7,9 @@ Translate::Translate(float newX, float newY, float newZ){
 }
 
 Translate::Translate(XMLElement *elem){
-    
+    x = atof((char *)elem->Attribute("x"));
+    y = atof((char *)elem->Attribute("y"));
+    z = atof((char *)elem->Attribute("z"));
 }
 
 
@@ -18,6 +20,13 @@ Rotate::Rotate(float newX, float newY, float newZ, float newAngle){
     angle = newAngle;    
 }
 
+Rotate::Rotate(XMLElement *elem){
+    x = atof((char *)elem->Attribute("x"));
+    y = atof((char *)elem->Attribute("y"));
+    z = atof((char *)elem->Attribute("z"));
+    angle = atof((char *)elem->Attribute("angle"));
+}
+
 
 Scale::Scale(float newX, float newY, float newZ){
     x = newX;
@@ -25,3 +34,8 @@ Scale::Scale(float newX, float newY, float newZ){
     z = newZ;
 }
 
+Scale::Scale(XMLElement *elem){
+    x = atof((char *)elem->Attribute("x"));
+    y = atof((char *)elem->Attribute("y"));
+    z = atof((char *)elem->Attribute("z"));
+}

2a1f526cb13c0965158ee1e1b8276f0de33a3127
Guilherme Varela <guilherme.varelaaa@gmail.com>
2023-03-18 17:15:17 +0000

    remake world, group, transform e model

--- phase2/include/engine/group.h
+++ phase2/include/engine/group.h
@@ -4,11 +4,15 @@
 #include <vector>
 #include <string.h>
 #include "vbo.h"
+#include "model.h"
+#include "transform.h"
 
 class Group{
     public:
-        vector<string> modelsPaths;
-        vector<VBO> vbos;
+        vector<Group> groups;
+        vector<Model> models;
+        vector<Transform> transforms;
+        
         Group();
         Group(vector<VBO> vbos);
         Group(vector<string> modelsPaths);
--- phase2/src/utils/model.cpp
+++ phase2/src/utils/model.cpp
@@ -1,32 +1,58 @@
 #include "../../include/model.h"
 
-Model::Model(vector<Point> newVertices, vector<Triangle> newTriangles){
-    vertices = newVertices;
-    triangles = newTriangles;
-}
+Model::Model(char* path){
+    glGenBuffers(1, &vertices_buffer);
+    glGenBuffers(1, &indexes_buffer);
 
+    vector<float> points;
+    vector<int> indexes;
 
-void Model::toFile(char* path){
-    ofstream file; 
-    file.open(path);
-    char buffer[1024];
+    ifstream file(path);
+    string line;
+    getline(file, line);
     
-    int nVertices = vertices.size(), nTriangles = triangles.size();
+    stringstream ss(line);
+    string nVerticesStr, nTrianglesStr;
+    ss >> nVerticesStr >> nTrianglesStr;
 
-    //First line (nVertices e nTriangles)
-    file << nVertices << " " << nTriangles << "\n";
-
-    //Vertices
+    nVertices = stoi(nVerticesStr);
+    int nTriangles = stoi(nTrianglesStr);
+    
     for(int i = 0; i < nVertices; i++){
-        sprintf(buffer, "%f %f %f\n", vertices[i].x, vertices[i].y, vertices[i].z);
-        file << buffer;
+        getline(file, line);
+        stringstream ss(line);
+        string x, y, z;
+        ss >> x >> y >> z;
+        points.push_back(stof(x));
+        points.push_back(stof(y));
+        points.push_back(stof(z));
     }
 
-    //Triangles
     for(int i = 0; i < nTriangles; i++){
-        sprintf(buffer, "%d %d %d\n", triangles[i].indP1, triangles[i].indP2, triangles[i].indP3);
-        file << buffer;
+        getline(file, line);
+        stringstream ss(line);
+        string indP1, indP2, indP3;
+        ss >> indP1 >> indP2 >> indP3;
+        indexes.push_back(stoi(indP1));
+        indexes.push_back(stoi(indP2));
+        indexes.push_back(stoi(indP3));
     }
 
     file.close();
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertices_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * points.size(), points.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexes_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * indexes.size(), indexes.data(), GL_STATIC_DRAW);
+
+    nIndexes = indexes.size();
+}
+
+void Model::draw(){
+    glBindBuffer(GL_ARRAY_BUFFER, vertices_buffer);
+    glVertexPointer(3,GL_FLOAT,0,0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexes_buffer);
+    glDrawElements(GL_TRIANGLES, nIndexes, GL_UNSIGNED_INT, NULL);
 }

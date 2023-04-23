d1533b8c853f91fba3c616f40061862bff1d551f
Guilherme Varela <guilherme.varelaaa@gmail.com>
2023-04-23 01:55:31 +0100

    beaier h

---
+++ phase3/include/generator/bezier.h
@@ -0,0 +1,15 @@
+#ifndef __BEZIER__
+#define __BEZIER__
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "../point.h"
+#include "../triangle.h"
+
+using namespace std;
+
+pair<vector<Point>, vector<Triangle> > generateBezier(char *path, int level);
+
+#endif

3d37ab54ff0fe7c73ff3b983642bdd2c2955dc73
Guilherme Varela <guilherme.varelaaa@gmail.com>
2023-04-28 22:47:06 +0100

    script em condições com rotaçoes sobbre os eixos

--- phase3/src/utils/orbit_points.py
+++ phase3/src/utils/orbit_points.py
@@ -1,10 +1,15 @@
 from math import sin, cos, pi
 
 step = (2*pi)/15
+yRotation = pi/4
+xRotation = pi/4
 a = 9
 b = 12
+centerx = 0
+centerz = 0
 
 for i in range(0,15):
-    x = a * cos(i*step)
-    y = b * sin(i*step)
-    print('<point x="' + str(x) + '" y="0" z="' + str(y) + '"/>')
+    x = centerx + a * cos(yRotation) * cos(i*step) - b * sin(yRotation) * sin(i*step) * cos(xRotation)
+    y = b * sin(i*step) * sin(xRotation)
+    z = centerz + a * sin(yRotation) * cos(i*step) + b * cos(yRotation) * sin(i*step) * cos(xRotation)
+    print('<point x="' + str(x) + '" y="' + str(y) + '" z="' + str(z) + '"/>')
